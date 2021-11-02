b8902bbc95dda7076752c10d7d698a1996b2ee55
irfannudin02 <muhamadirfannudin3@gmail.com>
2021-11-02 12:18:31 +0700

    menambah file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en"
+<head>
+    <meta charset="UTF-8"
+    <meta http-equiv="x-UA-compatible" content="IE=edge">
+    <meta name="viewport" content="widht=device-widht"
+    <title>MDPL_Prak_p6</title>
+</head>
+<body>
+    <h3>Materi Git remote<h3/>
+</body>
