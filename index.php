77631b662166c7a49c5fb0e539d9e70b4e693bed
irfannudin02 <muhamadirfannudin3@gmail.com>
2021-11-02 12:27:45 +0700

    menambahkan List

--- index.php
+++ index.php
@@ -9,4 +9,8 @@
 <body>
     <h3>Materi Git remote<h3/>
     <p>Repo untuk mencoba Git remote. Kuliah mdpl prak VIII</P>
+    <ul>
+        <li>Materi 1</li>
+        <li>Materi 2</li>
+    </ul>
 </body>
